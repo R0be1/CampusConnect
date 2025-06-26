6ca8b14db9a5a1ab585a0aad495f5d2c4386a518
Biruk Zegeju <biruk.zegeju@gmail.com>
2025-06-26 14:44:37 +0000

    Add a way to add mockup exams without relting the result with student re

--- src/app/dashboard/results/page.tsx
+++ src/app/dashboard/results/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { ClipboardCheck, Edit, CheckCircle, ArrowRight } from "lucide-react";
