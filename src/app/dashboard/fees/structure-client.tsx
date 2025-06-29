6749717a1f3dd71237a934b740c47a4c1881da88
R0be1 <robelas2001@gmail.com>
2025-06-29 18:11:21 +0000

    Error: AlertDialogFooter is not defined
    src\app\dashboard\fees\structur

--- src/app/dashboard/fees/structure-client.tsx
+++ src/app/dashboard/fees/structure-client.tsx
@@ -18,7 +18,7 @@ import { Separator } from "@/components/ui/separator";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useToast } from "@/hooks/use-toast";
 import { createFeeStructureAction, updateFeeStructureAction, deleteFeeStructureAction, createPenaltyRuleAction, updatePenaltyRuleAction, deletePenaltyRuleAction } from "./actions";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 
 // Schemas
 const feeStructureSchema = z.object({
